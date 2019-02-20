31f412347092fa6ac311153aac06291ec5cafd55
HackerRank <hackerrank@example.com>
2019-02-20 15:32:38 +0000

    Saved at Wed Feb 20 2019 09:32:36 GMT-0600 (Central Standard Time)

--- spring-hack-couponmicroservice/src/main/java/com/hackerrank/sample/controller/ModelController.java
+++ spring-hack-couponmicroservice/src/main/java/com/hackerrank/sample/controller/ModelController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class ModelController {
     @Autowired
     private ModelService modelService;
