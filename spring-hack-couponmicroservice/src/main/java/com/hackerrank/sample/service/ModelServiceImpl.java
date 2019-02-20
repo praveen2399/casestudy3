c62474526642dd7c60bbd39c2c640f739b593773
HackerRank <hackerrank@example.com>
2019-02-20 15:34:08 +0000

    Saved at Wed Feb 20 2019 09:34:06 GMT-0600 (Central Standard Time)

--- spring-hack-couponmicroservice/src/main/java/com/hackerrank/sample/service/ModelServiceImpl.java
+++ spring-hack-couponmicroservice/src/main/java/com/hackerrank/sample/service/ModelServiceImpl.java
@@ -10,43 +10,5 @@ import org.springframework.stereotype.Service;
 
 @Service("modelService")
 public class ModelServiceImpl implements ModelService {
-    @Autowired
-    private ModelRepository modelRepository;
-
-    @Override
-    public void deleteAllModels() {
-        modelRepository.deleteAllInBatch();
-    }
-
-    @Override
-    public void deleteModelById(Long id) {
-        modelRepository.deleteById(id);
-    }
-
-    @Override
-    public void createModel(Model model) {
-        Model existingModel = modelRepository.findOne(model.getId());
-
-        if (existingModel != null) {
-            throw new BadResourceRequestException("Model with same id exists.");
-        }
-
-        modelRepository.save(model);
-    }
-
-    @Override
-    public Model getModelById(Long id) {
-        Model model = modelRepository.findOne(id);
-
-        if (model == null) {
-            throw new NoSuchResourceFoundException("No model with given id found.");
-        }
-
-        return model;
-    }
-
-    @Override
-    public List<Model> getAllModels() {
-        return modelRepository.findAll();
-    }
+   
 }
