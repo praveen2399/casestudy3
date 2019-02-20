8aee63f7b8da338a7f96ede65335c87a00c3d392
HackerRank <hackerrank@example.com>
2019-02-20 15:33:55 +0000

    Saved at Wed Feb 20 2019 09:33:54 GMT-0600 (Central Standard Time)

--- spring-hack-couponmicroservice/src/main/java/com/hackerrank/sample/service/ModelService.java
+++ spring-hack-couponmicroservice/src/main/java/com/hackerrank/sample/service/ModelService.java
@@ -4,13 +4,6 @@ import com.hackerrank.sample.model.Model;
 import java.util.List;
 
 public interface ModelService {
-    void deleteAllModels();
-    void deleteModelById(Long id);
-
-    void createModel(Model model);
-
-    Model getModelById(Long id);
-
-    List<Model> getAllModels();
-
+   
+    
 }
